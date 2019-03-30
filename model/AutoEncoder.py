1c55f866e9b05dc39f6805a20fb6396162385ec2
兮尘 <761516186@qq.com>
2019-03-30 10:42:10 +0800

    data

--- model/AutoEncoder.py
+++ model/AutoEncoder.py
@@ -5,7 +5,6 @@ from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
-from torchvision.datasets import MNIST
 from torchvision.utils import save_image
 
 import pandas as pd
@@ -13,10 +12,12 @@ import numpy as np
 
 if not os.path.exists('./mlp_img'):
     os.mkdir('./mlp_img')
-
+if not os.path.exists('./filters'):
+    os.mkdir('./filters')
+device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
 
 def to_img(x):
-    x = x.view(x.size(0), 1, 28, 28)
+    x = x.view(x.size(0), 1, 100, 50)
     return x
 
 num_epochs = 20
@@ -31,7 +32,7 @@ def add_noise(img):
 
 
 def plot_sample_img(img, name):
-    img = img.view(1, 28, 28)
+    img = img.view(1, 100, 50)
     save_image(img, './sample_{}.png'.format(name))
 
 
@@ -49,14 +50,14 @@ def tensor_round(tensor):
 
 class SimulatedDataset(Dataset):
     '''
-    每一个 Item 是 (5000, 1) 的列向量
+    每一个 Item 是 (5000, ) 的向量
     '''
     def __init__(self, csv_data_path):
         self.csv_data_path = csv_data_path
         self.csv_data = pd.read_csv(csv_data_path)
 
     def __len__(self):
-        return len(self.csv_data.columns)
+        return len(self.csv_data.columns) - 1
 
     def __getitem__(self, index):
         a_column_of_data = self.csv_data.iloc[:, index+1]
@@ -67,8 +68,7 @@ img_transform = transforms.Compose([
     transforms.Lambda(lambda tensor:min_max_normalization(tensor, 0, 1)),
     transforms.Lambda(lambda tensor:tensor_round(tensor))
 ])
-
-dataset = MNIST('./data', transform=img_transform, download=True)
+dataset = SimulatedDataset("./data/counts_simulated_dataset1_dropout0.05.csv")
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 
@@ -76,14 +76,14 @@ class AutoEncoder(nn.Module):
     def __init__(self):
         super(AutoEncoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(28 * 28, 256),
+            nn.Linear(5000, 256),
             nn.ReLU(True),
             nn.Linear(256, 64),
             nn.ReLU(True))
         self.decoder = nn.Sequential(
             nn.Linear(64, 256),
             nn.ReLU(True),
-            nn.Linear(256, 28 * 28),
+            nn.Linear(256, 5000),
             nn.Sigmoid())
 
     def forward(self, x):
@@ -92,18 +92,19 @@ class AutoEncoder(nn.Module):
         return x
 
 
-model = AutoEncoder().cuda()
+model = AutoEncoder().to(device)
 criterion = nn.BCELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
 
 for epoch in range(num_epochs):
     for data in dataloader:
-        img, _ = data
+        img = data
         img = img.view(img.size(0), -1)
-        noisy_img = add_noise(img)
-        noisy_img = Variable(noisy_img).cuda()
-        img = Variable(img).cuda()
+        noisy_img = add_noise(img.float())
+        noisy_img = Variable(noisy_img).float().to(device)
+        img = Variable(img).float().to(device)
         # ===================forward=====================
+        print(noisy_img.size())
         output = model(noisy_img)
         loss = criterion(output, img)
         MSE_loss = nn.MSELoss()(output, img)
@@ -112,8 +113,11 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
     # ===================log========================
-    print('epoch [{}/{}], loss:{:.4f}, MSE_loss:{:.4f}'
-          .format(epoch + 1, num_epochs, loss.data[0], MSE_loss.data[0]))
+    print('epoch [{}/{}], loss:{:.4f}, MSE_loss:{:.4f}'.format(
+        epoch + 1,
+        num_epochs,
+        loss.data,
+        MSE_loss.data))
     if epoch % 10 == 0:
         x = to_img(img.cpu().data)
         x_hat = to_img(output.cpu().data)
